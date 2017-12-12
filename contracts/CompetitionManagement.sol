f2164d770793c846fed57f7fa0bdbb313db83981
MelnikovAlmaz <melnikovalmaz@gmail.com>
2017-12-12 16:47:38 +0300

    Save Event addresses instead Events

--- contracts/CompetitionManagement.sol
+++ contracts/CompetitionManagement.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.18;
 import "./Event.sol";
 //["0xca35b7d915458ef540ade6068dfe2f44e8fa733c",  "0x14723a09acff6d2a60dcdf7aa4aff308fddc160c", "0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db"]
+//"A", 124, "B", ["0xca35b7d915458ef540ade6068dfe2f44e8fa733c"], []
+//"0xca35b7d915458ef540ade6068dfe2f44e8fa733c", "A", 124, "B", ["0xca35b7d915458ef540ade6068dfe2f44e8fa733c", "0x14723a09acff6d2a60dcdf7aa4aff308fddc160c"], []
 contract CompetitionManagement{
     // Constants
     uint NOMINATION_TIME = 1 days;
@@ -31,7 +33,7 @@ contract CompetitionManagement{
     mapping(address => User) _users;
     mapping(address => bool) _experts;
     
-    Event[] _events;
+    address[] _events;
     
     function CompetitionManagement(address[] creators) public {
         // Number of experts should be greater/equal 3
@@ -42,33 +44,39 @@ contract CompetitionManagement{
             _experts[creators[i]] = true;
         }
     }
-    
-    function createEvent(uint competence_start_date, string competence_name, address[] expert_list, address[] participant_list) public onlyExpert {
+
+    function createEvent(string name, uint competence_start_date, string competence_name, address[] expert_list, address[] participant_list) public onlyExpert {
         // Check all requirements
-        // All addresses should be experts
+        // All addresses should be experts && creator in expert list
+        bool isExpertInList = false;
         for (uint i=0; i<expert_list.length;i++){
             require(_experts[expert_list[i]] == true);
+            if(expert_list[i] == msg.sender){
+                isExpertInList = true;
+            }
         }
+        require(isExpertInList);
+
         // All participants should not be experts && registered ?
-        for (i=0; i<expert_list.length;i++){
+        for (i=0; i<participant_list.length;i++){
             require(_experts[participant_list[i]] == false);
         }
         // Form Event data structure
-        Event newEvent = new Event(competence_name, now, competence_start_date, competence_name, expert_list, participant_list);
-        
+        Event newEvent = new Event(name, now, competence_start_date, competence_name, expert_list, participant_list);
+
         // Add new Event
-        _events.push(newEvent);
+        _events.push(address(newEvent));
     }
 
     function changeEvent(address event_address, string name, uint start_date, string competence, address[] expert_list, address[] participant_list) public{
-        Event currentEvent = findEvent(event_address);
+        Event currentEvent = Event(findEvent(event_address));
         currentEvent.changeEvent(msg.sender, name, start_date, competence, expert_list, participant_list);
 
     }
-        // Modifiers
-    function findEvent(address event_address) private returns(Event){
+    // Modifiers
+    function findEvent(address event_address) private returns(address){
         for(uint i=0; i<_events.length; i++){
-            if(address(_events[i]) == event_address){
+            if(_events[i] == event_address){
                 return _events[i];
             }
         }
