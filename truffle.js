e3deb888c8afc277fdccc821d4102b8c45f78093
MelnikovAlmaz <melnikovalmaz@gmail.com>
2017-12-13 03:29:24 +0300

    Configure simple node server

--- truffle.js
+++ truffle.js
@@ -1,4 +1,26 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
+
+// берём Express
+var express = require('express');
+
+// создаём Express-приложение
+var app = express();
+
+// создаём маршрут для главной страницы
+// http://localhost:8080/
+app.get('/', function(req, res) {
+  res.sendfile('src/index.html');
+});
+
+// запускаем сервер на порту 8080
+app.listen(8485);
+// отправляем сообщение
+console.log('Сервер стартовал!');
